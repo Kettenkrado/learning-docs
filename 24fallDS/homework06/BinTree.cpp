82a52142dd4b6be06e91dcd8316511b5def60f22
Nameless <575820784@qq.com>
2024-11-09 17:02:26 +0800

    11.9

---
+++ 24fallDS/homework06/BinTree.cpp
@@ -0,0 +1,172 @@
+//VS中使用传统C不安全的函数 
+#define _CRT_SECURE_NO_WARNINGS  
+
+#include <stdlib.h>
+#include <iostream>
+#include "BinTree.h"
+using namespace std;
+
+int main() 
+{
+
+	BiTree bt1 = NULL;
+	BiTree bt2 = NULL;
+	
+	char sTree1[] = "BC!D!!E!!";
+	char sTree2[] = "FGI!!!HJ!!K!!";
+	
+	cout << "\n从先序遍历结果  " << sTree1 << "  创建第一棵二叉树\n";
+	int nStringStart = 0;
+	bt1 = CreateBinTree(sTree1, nStringStart);
+	
+	cout << "\n第一棵树的先序遍历：\n";
+	PreOrderTraverse(bt1);
+	cout << "\n第一棵树的中序遍历：\n";
+	InOrderTraverse(bt1);
+	cout << "\n第一棵树的后序遍历：\n";
+	PostOrderTraverse(bt1);
+	cout << "\n第一棵树的层序遍历：\n";
+	LevelOrderTraverse(bt1);
+	cout << "\n第一棵树的结点数为："
+              << BiTreeNodeCount(bt1)
+			  << "\n\n第一棵树的叶子结点数为："
+              << BiTreeLeavesCount(bt1)
+			  << "\n\n第一棵树的层数为："
+              << BiTreeLevelCount(bt1)
+			  << "\n";
+	
+	cout << "\n从先序遍历结果  " << sTree2 << "  创建第二棵二叉树\n";
+	nStringStart = 0;
+	bt2 = CreateBinTree(sTree2, nStringStart);
+	
+	cout << "\n第二棵树的先序遍历：\n";
+	PreOrderTraverse(bt2);
+	cout << "\n第二棵树的中序遍历：\n";
+	InOrderTraverse(bt2);
+	cout << "\n第二棵树的后序遍历：\n";
+	PostOrderTraverse(bt2);
+	cout << "\n第二棵树的层序遍历：\n";
+	LevelOrderTraverse(bt2);
+	cout << "\n第二棵树的结点数为："
+              << BiTreeNodeCount(bt2)
+			  << "\n\n第二棵树的叶子结点数为："
+              << BiTreeLeavesCount(bt2)
+			  << "\n\n第二棵树的层数为："
+              << BiTreeLevelCount(bt2)
+			  << "\n";
+
+	cout << "\n将两棵树合并为一棵以A为根的新二叉树，第一棵树为A的左子树，第二棵为A的右子树\n";
+	BiTree newBT = CreateBinTree('A', bt1, bt2);
+	
+	cout << "\n新树的先序遍历：\n";
+	PreOrderTraverse(newBT);
+	cout << "\n新树的中序遍历：\n";
+	InOrderTraverse(newBT);
+	cout << "\n新树的后序遍历：\n";
+	PostOrderTraverse(newBT);
+	cout << "\n新树的层序遍历：\n";
+	LevelOrderTraverse(newBT);
+	cout << "\n新树的结点数为："
+              << BiTreeNodeCount(newBT)
+			  << "\n\n新树的叶子结点数为："
+              << BiTreeLeavesCount(newBT)
+			  << "\n\n新树的层数为："
+              << BiTreeLevelCount(newBT)
+			  << "\n";
+
+	//销毁bt1，bt2 
+	cout << "\n将第一棵树销毁\n";
+	DestroyBinTree(bt1);
+	cout << "\n将第二棵树销毁\n";
+	DestroyBinTree(bt2);
+}
+	
+
+//1. 初始化二叉链式存储的二叉树，采用无头结点的方式初始化 
+BiTree InitBinTree()
+{
+	BiTNode *bt;
+    bt = NULL;
+    return bt;
+}
+
+//2. 销毁二叉链式存储的二叉树，释放每个结点所分配的内存 
+//   注意是没有头结点的二叉树 
+bool DestroyBinTree(BiTree bt)
+{
+	//将下面的代码修改为正确的代码 
+
+	return false;
+} 
+
+//3.1 从先序遍历的字符串中创建二叉树，将创建的二叉树用返回值返回到调用者 
+//    先序遍历的字符串中用字符'!'表示空结点 
+BiTree CreateBinTree(char * preordString, int & nStringStart)
+{
+	//将下面的代码修改为正确的代码 
+	return NULL;
+}
+
+// 3.3
+BiTree CreateBinTree(ElemType rootVal, BiTree leftChild, BiTree rightChild) {
+    BiTree p = (BiTree)malloc(sizeof(BiTNode));
+}
+
+//4.1 先序遍历二叉树，将结果输出到控制台（stdc::out）
+//   返回值：空树返回false，非空树返回true 
+bool PreOrderTraverse(BiTree bt)
+{
+	//将下面的代码修改为正确的代码 
+	return false;
+}
+
+//4.2 中序遍历二叉树，将结果输出到控制台（stdc::out）
+//   返回值：空树返回false，非空树返回true 
+bool InOrderTraverse(BiTree bt)
+{
+	//将下面的代码修改为正确的代码 
+	return false;
+}
+
+//4.3 后序遍历二叉树，将结果输出到控制台（stdc::out）
+//   返回值：空树返回false，非空树返回true 
+bool PostOrderTraverse(BiTree bt)
+{
+	//将下面的代码修改为正确的代码 
+	return false;
+}
+
+
+//4.4 层序遍历二叉树，将结果输出到控制台（stdc::out）
+//   返回值：空树返回false，非空树返回true 
+bool LevelOrderTraverse(BiTree bt)
+{
+	//将下面的代码修改为正确的代码 
+	return false;
+}
+
+
+//5 计算树的结点数目
+int BiTreeNodeCount(BiTree bt)
+{
+	//将下面的代码修改为正确的代码 
+	return 0;
+}
+
+
+//6 计算树的叶子结点数目
+int BiTreeLeavesCount(BiTree bt)
+{
+	//将下面的代码修改为正确的代码 
+	return 0;
+}
+
+
+//7 计算树的层数 
+int BiTreeLevelCount(BiTree bt)
+{
+	//将下面的代码修改为正确的代码 
+	return 0;
+}
+
+ 
---
+++ 24fallDS/homework06/BinTree.h
@@ -0,0 +1,61 @@
+#ifndef HOMEWORK06_BINTREE_H
+#define HOMEWORK06_BINTREE_H
+
+typedef char ElemType;
+
+//二叉树的链式存储实现，基本操作函数定义
+// 参考严蔚敏教材，P127，定义结点的类型
+//结点的存储结构
+typedef struct BiTNode
+{
+    ElemType  data;
+    struct BiTNode  *left, *right;	//左右孩子指针
+} BiTNode, *BiTree;//
+
+//1. 初始化二叉链式存储的二叉树，采用无头结点的方式初始化
+BiTree InitBinTree();
+
+//2. 销毁二叉链式存储的二叉树，释放每个结点所分配的内存
+//   注意是没有头结点的二叉树
+bool DestroyBinTree(BiTree bt);
+
+//3.1 从先序遍历的字符串中创建二叉树，将创建的二叉树用返回值返回到调用者
+//    先序遍历的字符串中用字符'!'表示空结点
+//参考教材 P131 实现，这里的参数nStringStart，表示创建树时，从preordString的那个字符开始读入
+//使用引用形式的参数传递方式进行参数传递，这样可以在递归调用时，每读入一个字符后，
+// nStringStart向后移动一个位置，并将这个值的改变带回到调用者
+BiTree CreateBinTree(char * preordString, int & nStringStart);
+
+//3.1 从先序遍历的字符串中创建二叉树，将创建的二叉树用返回值返回到调用者
+//    先序遍历的字符串中用字符'!'表示空结点
+//   这个是采用指针传递参数的，有兴趣的同学可以试着实现一下这个
+BiTree CreateBinTree(char * preordString, int * pnStringStart);
+
+BiTree CreateBinTree(ElemType rootVal, BiTree leftChild, BiTree rightChild);
+
+//4.1 先序遍历二叉树，将结果输出到控制台（stdc::out）
+//   返回值：空树返回false，非空树返回true
+bool PreOrderTraverse(BiTree bt);
+
+//4.2 中序遍历二叉树，将结果输出到控制台（stdc::out）
+//   返回值：空树返回false，非空树返回true
+bool InOrderTraverse(BiTree bt);
+
+//4.3 后序遍历二叉树，将结果输出到控制台（stdc::out）
+//   返回值：空树返回false，非空树返回true
+bool PostOrderTraverse(BiTree bt);
+
+//4.4 层序遍历二叉树，将结果输出到控制台（stdc::out）
+//   返回值：空树返回false，非空树返回true
+bool LevelOrderTraverse(BiTree bt);
+
+//5 计算树的结点数目
+int BiTreeNodeCount(BiTree bt);
+
+//6 计算树的叶子结点数目
+int BiTreeLeavesCount(BiTree bt);
+
+//7 计算树的层数
+int BiTreeLevelCount(BiTree bt);
+
+#endif //HOMEWORK06_BINTREE_H
